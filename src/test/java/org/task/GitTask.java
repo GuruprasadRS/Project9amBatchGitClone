ce376e5ed8f56293898786f0448787f96eb1116d
GuruprasadRS <guruprasad.off.use@gmail.com>
2021-04-14 11:08:30 +0530

    Newly Modified Codes

--- src/test/java/org/task/GitTask.java
+++ src/test/java/org/task/GitTask.java
@@ -3,6 +3,9 @@ package org.task;
 public class GitTask {
 private void bank() {
 	System.out.println("saving");
+	System.out.println("Fixed");
+	System.out.println("Deposit");
+	System.out.println("New Lines Of Codes Modified Successfully");
 }
 public static void main(String[] args) {
 	GitTask g=new GitTask();
