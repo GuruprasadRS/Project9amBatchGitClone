33a1545fb74099ae4aa522a0571f699277b2be3a
GuruprasadRS <guruprasad.off.use@gmail.com>
2021-04-14 10:51:30 +0530

    First Day Task

---
+++ src/test/java/org/task/GitTask.java
@@ -0,0 +1,11 @@
+package org.task;
+
+public class GitTask {
+private void bank() {
+	System.out.println("saving");
+}
+public static void main(String[] args) {
+	GitTask g=new GitTask();
+	g.bank();
+}
+}
